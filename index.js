ffecfef27a9f5b51e47002b6b0cdaa0ad6f70fec
fancyfanding <2512356147@qq.com>
2026-03-02 08:58:52 +0800

    自己尝试的提交一次

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+let e = "pwwkew";
+
+var trap = function (e) {
+    let now;
+    for(let i=0;i<e.length;i++){
+        console.log(e[i]);
+    }
+};
+console.log(trap(e));
