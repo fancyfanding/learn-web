b9621eb3aa58850eb37b4c544fb88fb5468573ce
fancyfanding <2512356147@qq.com>
2026-03-27 23:19:50 +0800

    词法作用域作用域链和闭包

--- 面试题/deepCopy.js
+++ 面试题/deepCopy.js
@@ -15,12 +15,12 @@
  * 方法：...
  * 会创建一个新对象
  */
-const obj1 = {
-	name: "fancy",
-	city: {
-		area: 2,
-	},
-};
+// const obj1 = {
+// 	name: "fancy",
+// 	city: {
+// 		area: 2,
+// 	},
+// };
 
 // const obj2 = { ...obj1 };
 // obj2.name = "fanding";
@@ -39,21 +39,82 @@ const obj1 = {
 // console.log(obj3);
 
 // function deepClon(obj) {
-//     if(obj===null||typeof obj !== "object")
-//     let newObj = {}
-
+// 	let newObj = Array.isArray(obj) ? [] : {};
+// 	if (typeof obj === null || typeof obj !== "object") return obj;
+// 	for (let key in obj) {
+// 		if (obj[key] === obj) continue;
+// 		if (obj.hasOwnProperty(key)) {
+// 			newObj[key] = deepClon(obj[key]);
+// 		}
+// 	}
+// 	return newObj;
 // }
 //实现一个deepCopy
 //其他类型如何处理，数组、函数
 //深拷贝中如何解决循环引用的问题，属性引用自身，导致栈溢出
 
-(() => {
-	let a = 1;
-	function b() {
-		let c = a;
-		console.log(c);
-	}
-	b();
-})();
+// (() => {
+// 	let a = 1;
+// 	function b() {
+// 		let c = a;
+// 		console.log(c);
+// 	}
+// 	b();
+// })();
 
 //作用域链，词法作用域，闭包的能力及使用场景
+
+function fun1() {
+	console.log(fun2Value);
+}
+
+function fun2() {
+	let fun2Value = "hello world!";
+	fun1();
+}
+fun2();
+
+//词法作用域：作用域是在代码写下来的位置向外去寻找的，而不是他执行的位置
+//作用域链：当在当前作用域找不到使用的变量的时候，会逐层往外层作用域去寻找，直到全局，如果全局还找不到就是not defined
+//闭包：内部作用域引用了外部作用域的东西，构成了一种带走了当时定义的词法作用域，要用变量去干活，闭包不一定会造成内存泄漏（立即执行）
+		// 闭包的作用：数据私有化
+
+// function fun2() {
+// 	let fun2Value = "hello world!";
+// 	function fun1() {
+// 		console.log(fun2Value);
+// 	}
+// 	return fun1;
+// }
+
+// fun2()();
+// const res = fun2();
+
+// function make() {
+// 	let n = 0;
+// 	return () => ++n;
+// }
+// const c1 = make();
+// const c2 = make();
+
+// c1() 1
+// c2() 1
+// c1() 2
+// c1() 3
+
+function counter() {
+	let count = 0;
+	function get() {
+		return count;
+	}
+	function add() {
+		count++;
+	}
+	return {
+		get,
+		add
+	}
+}
+
+let op = counter();
+//课后作业：curring，weakmap（循环引用问题）
